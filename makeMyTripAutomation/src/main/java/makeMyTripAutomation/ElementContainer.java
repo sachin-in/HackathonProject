aea24477d32566c4d3b60ab0fab63afbf15a563a
Umashankar <umanick7@gmail.com>
2021-07-11 10:16:49 +0530

    new clone commit

--- makeMyTripAutomation/src/main/java/makeMyTripAutomation/ElementContainer.java
+++ makeMyTripAutomation/src/main/java/makeMyTripAutomation/ElementContainer.java
@@ -10,89 +10,128 @@ public class ElementContainer {
 
 	public static WebDriver driver;
 	public static WebElement element;
+	public static List<WebElement> elements;
 
-	// TO LOCATE "Cabs" tab
+	// login Button
 	public static WebElement loginButton(WebDriver driver) {
 		element = driver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[1]/div[1]/ul[1]/li[3]/div[1]"));
+		return element;
+	}
+
+	// gooogle login
+	public static WebElement googleLogin(WebDriver driver) {
+		element = driver
+				.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[1]/div[2]/div[2]/section[1]/div[4]"));
+		return element;
+	}
+
+	// enter email
+	public static WebElement emailInput(WebDriver driver) {
+		element = driver.findElement(By.id("identifierId"));
 		return element;
 
 	}
 
-	// TO LOCATE "Out station One Way" Radio button
-	public static WebElement outstationOneWay(WebDriver driver) {
-		element = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div/div[1]/ul/li[1]"));
+	// NextButton
+	public static WebElement nextButton(WebDriver driver) {
+		element = driver.findElement(By.xpath("//span[contains(text(),'Next')]"));
+		return element;
+
+	}
+
+	// enter password
+	public static WebElement passwordInput(WebDriver driver) {
+		element = driver.findElement(By.name("password"));
+		return element;
+
+	}
+
+	// 2 popup title
+	public static WebElement popupTitle(WebDriver driver) {
+		element = driver.findElement(By.xpath("//p[contains(text(),'Login/Signup for Best Prices')]"));
+		return element;
+
+	}
+
+	// login by google
+	public static WebElement googleClick(WebDriver driver) {
+		element = driver.findElement(By.xpath("//span[contains(text(),'Google')]"));
+		return element;
+
+	}
+
+	// 3 Webpage Signature
+	public static WebElement homepageSignature(WebDriver driver) {
+		element = driver.findElement(By.xpath("//p[contains(text(),'© 2021 MAKEMYTRIP PVT. LTD.')]"));
+		return element;
+
+	}
+
+	// 4 To locate the cab button
+	public static WebElement cabButton(WebDriver driver) {
+		element = driver.findElement(By.xpath("//span[@class='chNavText darkGreyText'][normalize-space()='Cabs']"));
 		return element;
 
 	}
 
 	// TO LOCATE "From" Drop down ListBox
 	public static WebElement fromCity(WebDriver driver) {
-		element = driver.findElement(By.id("fromCity"));
+		element = driver.findElement(By.xpath("//span[normalize-space()='From']"));
 		return element;
 	}
 
 	// TO SELECT THE CITY FROM THE SUGESSTIONS LIST BOX
-	public static WebElement selectDelhi(WebDriver driver, String city) {
-		element = driver.findElement(By.xpath("//span[@class=\"sr_city blackText\"and contains(text(),'"+city+"')][1]"));
-		return element;
-	}
-
-	// TO LOCATE "To" Drop down ListBox
-	public static WebElement toCity(WebDriver driver) {
-		element = driver.findElement(
-				By.xpath("//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div/div[2]/div[1]/div/div/div/input"));
+	public static WebElement selectDeparture(WebDriver driver) {
+		element = driver.findElement(By.xpath("//input[contains(@placeholder,'From')]"));
 		return element;
 	}
 
 	// TO SELECT THE CITY FROM THE SUGESSTIONS LIST BOX
-	public static WebElement selectManali(WebDriver driver, String city) {
-		element = driver
-				.findElement(By.xpath("//span[@class=\"sr_city blackText\"and contains(text(),'"+city+"')][1]"));
+	public static WebElement selectArrival(WebDriver driver) {
+		element = driver.findElement(By.xpath("//input[contains(@placeholder,'To')]"));
 		return element;
 	}
 
-	// TO LOCATE "Search" Button
-	public static WebElement searchCabs(WebDriver driver) {
-		element = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div/div[2]/p/a"));
+	// TO LOCATE "calender" Drop down ListBox
+	public static WebElement calender(WebDriver driver) {
+		element = driver.findElement(By.xpath("//span[contains(text(),'DEPARTURE')]"));
 		return element;
 	}
 
-	// TO LOCATE "Departure" Drop down ListBox
-	public static WebElement pickDay(WebDriver driver,String date) {
-		element = driver.findElement(By.xpath("(//div[text()='"+date+"'])[1]"));
+	// TO LOCATE day
+	public static List<WebElement> daySelect(WebDriver driver) {
+		elements = driver.findElements(By.className("DayPicker-Day"));
+		return elements;
+	}
+
+	// more dropdown
+	public static WebElement moreDropDown(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id='SW']/div[1]/div[2]/div/div/nav/ul/li[9]/a/span[1]"));
 		return element;
 	}
 
-	// TO LOCATE "SUV" CheckBox
-	public static WebElement suvCheckBox(WebDriver driver) {
-		element = driver.findElement(By.xpath("//*[contains(text(),'SUV')]"));
+	public static WebElement giftCard(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id='SW']/div[1]/div[2]/div/div/nav/ul/li[9]/div/a[4]"));
+		return element;
+	}
+	// ADULT List
+	// TO locate the hotel tab
+
+	public static WebElement hotelTab(WebDriver driver) {
+		element = driver.findElement(By.xpath("//li[@class='menu_Hotels']//span[2]"));
 		return element;
 	}
 
-	// TO FIND THE ELEMENTS OF ALL THE PRICES
-	public static List<WebElement> searchResults(WebDriver driver) {
-		List<WebElement> element = driver.findElements(By.xpath("//*[@class='font28 latoBlack blackText ']"));
+	// to locate the guests button
+	public static WebElement guestsButton(WebDriver driver) {
+		element = driver.findElement(By.id("guest"));
 		return element;
 	}
+	// the numbers available for adults
 
-	// TO LOCATE "Pickup Time" Drop down ListBox
-	public static WebElement pickUpTimeSelect(WebDriver driver, String time) {
-		element = driver.findElement(By.xpath("(//li[text()='"+time+"'])[1]"));
-		return element;
-
-	}
-
-	// TO FIND THE ELEMENT OF THE POP-UP
-	public static WebElement loginPopUp(WebDriver driver) {
-		element = driver.findElement(By.xpath("//*[@id='SW']/div[1]/div[1]/ul/li[5]/div[3]"));
-		return element;
-	}
-
-	// TO FIND THE ELEMENT OF THE LOGIN BUTTON
-	public static WebElement loginBtn(WebDriver driver) {
-
-		element = driver.findElement(By.xpath("//*[@id=\"SW\"]/div[1]/div[1]/ul/li[5]"));
-		return element;
+	public static List<WebElement> adultsList(WebDriver driver) {
+		elements = driver.findElements(By.xpath("//ul[@data-cy='adultCount']/li"));
+		return elements;
 	}
 
 }
--- makeMyTripAutomation/src/test/java/makeMyTripTests.java
+++ makeMyTripAutomation/src/test/java/makeMyTripTests.java
@@ -22,196 +22,193 @@ import com.aventstack.extentreports.ExtentReports;
 import makeMyTripAutomation.ElementContainer;
 
 public class makeMyTripTests {
-	
+
 	public static WebDriver driver;
 	public static String[][] data;
 	public static ExtentReports report;
-	
+
 	@BeforeTest
 	public void getWebDriver() {
-		driver=DriverSetup.createWebDriver("Drivers");
-		String baseURL=data[1][0];
+		driver = DriverSetup.createWebDriver("Drivers");
+		String baseURL = data[1][0];
 		driver.manage().window().maximize();
-		
 		driver.get(baseURL);
-		
-	}
-	@BeforeSuite
-	public String[][] getExcelData () throws IOException { //Reads data from the excel file
-		
-		/*Get Excel data from readExcelData class
-		Pass the size of data array as int type, name of excel file and name of excel sheet as string type*/
-		data=readExcelData.getData(7, "HackathonData.xlsx", "readData");
-		return data;	
-	}
-	@BeforeTest
-	public void createExtentReport() {
-		report=ExtentReport.createExtentReport("HackathonProject");	
 	}
 
-	@Test(priority=0)
+	@BeforeSuite
+	public String[][] getExcelData() throws IOException { // Reads data from the excel file
+
+		/*
+		 * Get Excel data from readExcelData class Pass the size of data array as int
+		 * type, name of excel file and name of excel sheet as string type
+		 */
+		data = readExcelData.getData(7, "HackathonData.xlsx", "readData");
+		return data;
+	}
+
+	@BeforeTest
+	public void createExtentReport() {
+		report = ExtentReport.createExtentReport("Hackathon");
+	}
+
+	@Test(priority = 0)
 	public void loginWithGmail() {
 		try {
-			ElementContainer.loginButton(driver).click();
+			ElementContainer.loginButton(driver).click();// login button
 			Thread.sleep(500);
-			driver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[1]/div[2]/div[2]/section[1]/div[4]")).click();//login by google
-			
+			ElementContainer.googleLogin(driver).click();// login by google
+
 			Set<String> handle = driver.getWindowHandles();
 			Iterator<String> it = handle.iterator();
 			String parentwindowid = it.next();
-			String childwindowid  =  it.next();
+			String childwindowid = it.next();
 			driver.switchTo().window(childwindowid);
 			Thread.sleep(1000);
-			
-			driver.findElement(By.id("identifierId")).sendKeys("bughunterss01@gmail.com");//enter email
-			driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();//click next
+
+			ElementContainer.emailInput(driver).sendKeys("bughunterss01@gmail.com");// enter email
+			ElementContainer.nextButton(driver).click();// click next
 			Thread.sleep(1000);
-			driver.findElement(By.name("password")).sendKeys("Bughunter$6");//enter password
-			driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();//click next
+			ElementContainer.passwordInput(driver).sendKeys("Bughunter$6");// enter password
+			ElementContainer.nextButton(driver).click();// click next
 			driver.switchTo().window(parentwindowid);
 			Thread.sleep(5000);
-			System.out.println("Logged in by google account");
-			
-		}
-		catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	@Test(priority=1)
+
+	@Test(priority = 1)
 	public void loginFromPopUp() {
 		try {
-		String popUp=driver.findElement(By.xpath("//p[contains(text(),'Login/Signup for Best Prices')]")).getText();//popup title
-		if(popUp.equalsIgnoreCase("Login/Signup for Best Prices")) {
-			driver.findElement(By.xpath("//span[contains(text(),'Google')]")).click();//login by google
-			Set<String> handle = driver.getWindowHandles();
-			Iterator<String> it = handle.iterator();
-			String parentwindowid = it.next();
-			String childwindowid  =  it.next();
-			driver.switchTo().window(childwindowid);
-			Thread.sleep(1000);
-			
-			driver.findElement(By.id("identifierId")).sendKeys("bughunterss01@gmail.com");//enter email
-			driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();//click next
-			Thread.sleep(1000);
-			driver.findElement(By.name("password")).sendKeys("Bughunter$6");//enter password
-			driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();//click next
-			driver.switchTo().window(parentwindowid);
-			Thread.sleep(5000);
-			System.out.println("Logged in with Google account");
-			
-		}
-		}
-		catch(Exception e) {
+			String popUp = ElementContainer.popupTitle(driver).getText();// popup title
+			if (popUp.equalsIgnoreCase("Login/Signup for Best Prices")) {
+				ElementContainer.googleClick(driver).click();// login by google
+				Set<String> handle = driver.getWindowHandles();
+				Iterator<String> it = handle.iterator();
+				String parentwindowid = it.next();
+				String childwindowid = it.next();
+				driver.switchTo().window(childwindowid);
+				Thread.sleep(1000);
+
+				ElementContainer.emailInput(driver).sendKeys("bughunterss01@gmail.com");// enter email
+				ElementContainer.nextButton(driver).click();// click next
+				Thread.sleep(1000);
+				ElementContainer.passwordInput(driver).sendKeys("Bughunter$6");// enter password
+				ElementContainer.nextButton(driver).click();// click next
+				driver.switchTo().window(parentwindowid);
+				Thread.sleep(5000);
+
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	@Test()
 	public void getHomePageSignature() {
-		String title=driver.findElement(By.xpath("//p[contains(text(),'© 2021 MAKEMYTRIP PVT. LTD.')]")).getText();//Web Page signature
+		String title = ElementContainer.homepageSignature(driver).getText();// Web Page signature
 		Assert.assertEquals(title, data[1][1], "Title is not correct");
 		Screenshot.captureScreenshot("Homepage", driver);
 	}
-	
-	@Test(priority=2)
+
+	@Test(priority = 2)
 	public void searchCab() {
-		driver.findElement(By.xpath("//span[@class='chNavText darkGreyText'][normalize-space()='Cabs']")).click();// Cab button
-        driver.findElement(By.xpath("//span[normalize-space()='From']")).click();//from option
-        driver.findElement(By.xpath("//input[contains(@placeholder,'From')]")).sendKeys("Delhi");//departure city
-        try {
+		ElementContainer.cabButton(driver).click();// Cab button
+		ElementContainer.fromCity(driver).click();// from option
+		ElementContainer.selectDeparture(driver).sendKeys("Delhi");// departure city
+		try {
 			Thread.sleep(500);
-		} catch(InterruptedException e) {
+		} catch (InterruptedException e) {
 		}
-        Actions keyPress=new Actions(driver);
-        keyPress.sendKeys(Keys.chord(Keys.ARROW_DOWN,Keys.ENTER)).perform();
-        driver.findElement(By.xpath("//input[contains(@placeholder,'To')]")).sendKeys("Manali");// arrival city
-        try {
+		Actions keyPress = new Actions(driver);
+		keyPress.sendKeys(Keys.chord(Keys.ARROW_DOWN, Keys.ENTER)).perform();
+		ElementContainer.selectArrival(driver).sendKeys("Manali");// arrival city
+		try {
 			Thread.sleep(500);
-		} catch(InterruptedException e) {
+		} catch (InterruptedException e) {
 		}
-        keyPress.sendKeys(Keys.chord(Keys.ARROW_DOWN,Keys.ENTER)).perform();
-        
-        driver.findElement(By.xpath("//span[contains(text(),'DEPARTURE')]")).click();// calender
-        List<WebElement> dates=driver.findElements(By.className("DayPicker-Day"));//day picker list
-      
-        for(int i=0;i<dates.size();i++) {
-            String test=dates.get(i).getText();
-        
-            if(test.equalsIgnoreCase("20")) {
-                dates.get(i).click();
-                break;
-            }
-        }
-        driver.findElement(By.xpath("//span[contains(text(),'PICKUP-TIME')]")).click();// pickup time
-        List<WebElement> time=driver.findElements(By.className("timeDropDown blackText"));
-        for(int j=0;j<time.size();j++) {
-        	String test2=time.get(j).getText();
-        	if(test2.equalsIgnoreCase("6:30")) {
-        		time.get(j).click();
-        	}
-        }
+		keyPress.sendKeys(Keys.chord(Keys.ARROW_DOWN, Keys.ENTER)).perform();
+		ElementContainer.calender(driver).click(); // calender
+		List<WebElement> dates = ElementContainer.daySelect(driver);// day picker list
+
+		for (int i = 0; i < dates.size(); i++) {
+			String test = dates.get(i).getText();
+
+			if (test.equalsIgnoreCase("20")) {
+				dates.get(i).click();
+				break;
+			}
+		}
+		driver.findElement(By.xpath("//span[contains(text(),'PICKUP-TIME')]")).click();// pickup time
+		List<WebElement> time = driver.findElements(By.className("timeDropDown blackText"));
+		for (int j = 0; j < time.size(); j++) {
+			String test2 = time.get(j).getText();
+			if (test2.equalsIgnoreCase("6:30")) {
+				time.get(j).click();
+			}
+		}
 	}
-	
-	@Test(dependsOnMethods="searchCab")
+
+	@Test(dependsOnMethods = "searchCab")
 	public void getCabDetails() {
-		
+
 	}
-	
+
 	@Test
 	public void buyGiftCard() throws InterruptedException {
-		Actions moveCursor=new Actions(driver);
-		moveCursor.moveToElement(driver.findElement(By.xpath("//*[@id=\"SW\"]/div[1]/div[2]/div/div/nav/ul/li[9]/a/span[1]"))).perform();
-		driver.findElement(By.xpath("//*[@id=\"SW\"]/div[1]/div[2]/div/div/nav/ul/li[9]/div/a[4]")).click();	
-		
+		Actions moveCursor = new Actions(driver);
+		moveCursor.moveToElement(ElementContainer.moreDropDown(driver)).perform();
+		ElementContainer.giftCard(driver).click();
 	}
-	
+
 	@Test
 	public void getAdultsList() {
-			
-			WebElement hotels=driver.findElement(By.xpath("//li[@class='menu_Hotels']//span[2]"));// hotels tab
-			Highlight.flash(hotels, driver);
-			hotels.click();
-			ArrayList<String> adults = new ArrayList<String>();
-	        WebElement guest=driver.findElement(By.id("guest"));// guest button
-	        Highlight.flash(guest, driver);
-	        guest.click();
-	        List <WebElement> adultsCount = driver.findElements(By.xpath("//ul[@data-cy='adultCount']/li"));//no. of adults list
-	        for (WebElement c : adultsCount)
-	        {
-	             adults.add(c.getText());
-	        }
-	        Assert.assertEquals(adults.size()+".0", data[1][2], "No. of adults is not correct");
+
+		WebElement hotels = ElementContainer.hotelTab(driver);// hotels tab
+		Highlight.flash(hotels, driver);
+		hotels.click();
+		ArrayList<String> adults = new ArrayList<String>();
+		WebElement guest = ElementContainer.guestsButton(driver);// guest button
+		Highlight.flash(guest, driver);
+		guest.click();
+		List<WebElement> adultsCount = ElementContainer.adultsList(driver);// no. of adults list
+		for (WebElement c : adultsCount) {
+			adults.add(c.getText());
+		}
+		Assert.assertEquals(adults.size() + ".0", data[1][2], "No. of adults is not correct");
 	}
-	
+
 	@AfterTest
 	public void closeBrowser() {
-		
-			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-		driver.quit();
-		
-	}
-	public static void invokeTestNG() {
-			
-			//Invoke TestNG to run as java application
-			TestNG test=new TestNG();
-			List<String> suites=Lists.newArrayList();
-			suites.add("testng.xml");
-			test.setTestSuites(suites);
-			test.run();	
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		driver.quit();
+
+	}
+
+	public static void invokeTestNG() {
+
+		// Invoke TestNG to run as java application
+		TestNG test = new TestNG();
+		List<String> suites = Lists.newArrayList();
+		suites.add("testng.xml");
+		test.setTestSuites(suites);
+		test.run();
+	}
+
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
-		//Call invokeTestNG method
-		//Project obj=new Project();
-  		makeMyTripTests.invokeTestNG();	
-}
-	
+
+		// Call invokeTestNG method
+		// Project obj=new Project();
+		makeMyTripTests.invokeTestNG();
+	}
+
 }
